2d3adef7dc1481fc4d9a0f2607dd6b4f491a982b
mikeyrayvon <mikey@interglobal.vision>
2018-08-29 16:33:08 -0500

    add image sizes

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,5 +8,11 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
+  add_image_size( 'height-900', 9999, 900, false );
+
+  add_image_size( 'height-1200', 9999, 1200, false );
+
+  add_image_size( 'width-640', 640, 9999, false );
+
   add_image_size( 'gallery', 1200, 9999, false );
 }
